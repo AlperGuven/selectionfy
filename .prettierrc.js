c73b5b0a9c456a78e799fafe748bdce29da99eda
Alper Guven <alper.guven@tarfin.com>
2025-02-21 11:34:39 +0300

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  trailingComma: "none",
+  arrowParens: "avoid",
+  tabWidth: 2,
+};
---
+++ src/components/BaseMultiselect.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="base-multiselect">
+    <p>Base Multiselect Component</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseMultiselect",
+};
+</script>
+
+<style scoped>
+.base-multiselect {
+  padding: 1rem;
+  border: 1px solid #ccc;
+}
+</style>
